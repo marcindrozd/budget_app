377a96ae1344588606d1ba1887a0788d2bc4222f
Marcin Drozd <marcindrozd@me.com>
2015-02-20 11:36:20 +0100

    Update dropdown for months

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :money_left, :current_user, :logged_in?
+  helper_method :money_left, :current_user, :logged_in?, :numeric_month
 
   MONTHS_TO_WORD = {'1' => 'January', '2' => 'February', '3' => 'March', '4' => 'April', '5' => 'May', '6' => 'June',
             '7' => 'July', '8' => 'August', '9' => 'September', '10' => 'October', '11' => 'November', '12' => 'December'}
 
-  MONTHS_TO_NUMBER = {'January' => '1', 'February' => '2', 'March' => '3', 'April' => '4', 'May' => '5', 'June' => '6',
-            'July' => '7', 'August' => '8', 'September' => '9', 'October' => '10', 'November' => '11', 'December' => '12'}
+  MONTHS_TO_NUMBER = {'January' => 1, 'February' => 2, 'March' => 3, 'April' => 4, 'May' => 5, 'June' => 6,
+            'July' => 7, 'August' => 8, 'September' => 9, 'October' => 10, 'November' => 11, 'December' => 12}
 
   def display_month(input)
     MONTHS_TO_WORD[input]
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,5 @@ class Expense < ActiveRecord::Base
   belongs_to :month
 
   validates :amount, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :name, presence: true
 end
